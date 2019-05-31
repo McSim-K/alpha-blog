ccb62dbd3f43293d0fd69995a48494857169c71d
Max <cerbereon@gmail.com>
2019-05-31 16:58:36 +0300

    Added styles for flash messages and flash errors

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
   	@article = Article.new(article_params)
   	if @article.save
-  	  flash[:notice] = "Your article was successfully created"
+  	  flash[:success] = "Your article was successfully created"
   	  redirect_to article_path(@article)
   	 else
   	 	render :new
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
 
   def update
   	if @article.update(article_params)
-  	  flash[:notice] = "Your article was successfully edited"
+  	  flash[:success] = "Your article was successfully edited"
   	  redirect_to article_path(@article)
   	 else
   	 	render :edit
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
   def destroy
   	@article.destroy
 
-  	flash[:notice] = "Your article was successfully deleted"
+  	flash[:danger] = "Your article was successfully deleted"
   	redirect_to articles_path
 
   end

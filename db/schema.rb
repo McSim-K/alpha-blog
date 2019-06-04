d4dbf72d36d09b1f1a71b329f83e5fc5a3804e28
Max <cerbereon@gmail.com>
2019-06-04 09:05:00 +0300

    Added assotioations between user and articles and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_044857) do
+ActiveRecord::Schema.define(version: 2019_06_04_054437) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
